4121afb38634a8fcb597df2d941e6b501d7d4b76
kotoleksii <akkot08@gmail.com>
2022-02-17 19:48:58 +0200

    fix: SecondTask

--- src/edu/itstep/SecondTask/CounterThread.java
+++ src/edu/itstep/SecondTask/CounterThread.java
@@ -1,5 +1,25 @@
 package edu.itstep.SecondTask;
 
-public class CounterThread {
-    
+public class CounterThread implements Runnable {
+    public static int count = 0;
+    int step = 1_000;
+    int tmp = count + step;
+
+    @Override
+    synchronized public void run() {
+        for (int i = 0; i < 1_000_000; i++) {
+            count++;
+            System.out.println(Thread.currentThread().getName() + " count = " + count);
+            if (count == tmp) {
+                try {
+                    tmp += step;
+                    notify();
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        notifyAll();
+    }
 }
--- src/edu/itstep/SecondTask/Main.java
+++ src/edu/itstep/SecondTask/Main.java
@@ -1,7 +1,14 @@
 package edu.itstep.SecondTask;
 
+
 public class Main {
     public static void main(String[] args) {
+        CounterThread counter = new CounterThread();
 
+        Thread thread1 = new Thread(counter);
+        Thread thread2 = new Thread(counter);
+
+        thread1.start();
+        thread2.start();
     }
 }
